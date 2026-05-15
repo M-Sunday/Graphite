dc0199b2a7970a6f6686dcc54cecdb9781e0d0fc
MSunday <arnausant2@gmail.com>
2026-05-15 10:43:54 +0200

    Initial Unity project setup with VisualScripting

---
+++ Assets/Editor/TerminalWindow.cs
@@ -0,0 +1,18 @@
+using UnityEditor;
+using UnityEngine;
+
+public class TerminalWindow : EditorWindow
+{
+    [MenuItem("Window/General/Terminal", false, 49)]
+    public static void ShowWindow()
+    {
+        var window = GetWindow<TerminalWindow>();
+        window.titleContent = new GUIContent("Terminal");
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Terminal", EditorStyles.boldLabel);
+    }
+}
